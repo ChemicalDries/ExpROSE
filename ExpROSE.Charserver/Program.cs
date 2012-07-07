d23e17cdd766edbf26d4e227206781f23d72d398
KingFurry <honor.world@gmail.com>
2012-07-07 15:02:19 +0200

    Initial stuff

---
+++ ExpROSE.Charserver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpROSE.Charserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
